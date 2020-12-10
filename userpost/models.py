7058043c0909fff791123b853e6a01c616aa9162
Kartik-21 <kartikjain2926@gmail.com>
2020-12-10 00:57:56 -0800

    fixed[object not found solved]

--- userpost/models.py
+++ userpost/models.py
@@ -5,7 +5,7 @@ from django.conf import settings
 # Create your models here.
 
 
-class Post(models.Model):
+class UserPost(models.Model):
     user_id = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     title = models.CharField(max_length=200, null=False, blank=False)
     time = models.DateTimeField(auto_now_add=True, null=True)
