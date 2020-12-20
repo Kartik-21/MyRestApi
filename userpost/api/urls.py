c39c5ec2fc789ad9dd12d00506f9e02c566c0ebd
Kartik-21 <kartikjain2926@gmail.com>
2020-12-20 23:00:17 +0530

    class based api added

--- userpost/api/urls.py
+++ userpost/api/urls.py
@@ -1,13 +1,16 @@
 from django.urls import path
 from rest_framework.urlpatterns import format_suffix_patterns
 
-from userpost.api.views import api_details_post_view, api_update_post_view, api_create_post_view, api_delete_post_view
+from userpost.api.views import api_details_post_view, api_update_post_view, api_create_post_view, api_delete_post_view, \
+    UserPostAPIView, UserPostCUDAPIView
 
 urlpatterns = [
     path(r'view/', api_details_post_view),
     path(r'update/<int:pk>/', api_update_post_view),
     path(r'create/', api_create_post_view),
     path(r'delete/<int:pk>/', api_delete_post_view),
+    path(r'v2/view/', UserPostAPIView.as_view()),
+    path(r'v2/update/<int:id>/', UserPostCUDAPIView.as_view())
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
